1ed0f98769b1cbffaf78761e89ef6cdfc392ae3d
Saurav Kumar <sauravkhdoolia@gmail.com>
2023-06-04 23:35:24 +0530

    feat: add loading to button

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,7 +1,22 @@
 import { css } from "@emotion/react";
 import Button from "../../shared/components/button";
+import { useCallback, useState } from "react";
+import useStore from "../flow-zone/store";
 
 const Header = () => {
+  const [saving, setSaving] = useState(false);
+  const { nodes, edges } = useStore((state) => ({
+    nodes: state.nodes,
+    edges: state.edges,
+  }));
+
+  const onSave = useCallback(() => {
+    setSaving(true);
+    setTimeout(() => {
+      setSaving(false);
+    }, 2000);
+  }, [nodes, edges]);
+
   return (
     <div
       css={css`
@@ -15,11 +30,8 @@ const Header = () => {
       `}
     >
       <p>React Flow Demo</p>
-      <Button
-        onClick={() => {
-          console.log("done");
-        }}
-      >
+
+      <Button loading={saving} onClick={onSave}>
         Save
       </Button>
     </div>
--- src/shared/components/button/index.tsx
+++ src/shared/components/button/index.tsx
@@ -1,9 +1,11 @@
 import { FC, MouseEventHandler, ReactNode } from "react";
 import { css } from "@emotion/react";
+import { Loader2 } from "lucide-react";
 
 interface ButtonProps {
   children: ReactNode;
   onClick: MouseEventHandler<HTMLButtonElement>;
+  loading: boolean;
 }
 
 const Button: FC<ButtonProps> = (props) => {
@@ -15,14 +17,35 @@ const Button: FC<ButtonProps> = (props) => {
         border-radius: 4px;
         background: #fff;
         cursor: pointer;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        line-height: 1.2;
 
         &:hover {
           background: #eee;
         }
       `}
+      disabled={props.loading}
       onClick={props.onClick}
     >
-      {props.children}
+      {props.loading ? (
+        <Loader2
+          size={18}
+          css={css`
+            color: black;
+            animation: spin 1s linear infinite;
+
+            @keyframes spin {
+              100% {
+                transform: rotate(360deg);
+              }
+            }
+          `}
+        />
+      ) : (
+        props.children
+      )}
     </button>
   );
 };
