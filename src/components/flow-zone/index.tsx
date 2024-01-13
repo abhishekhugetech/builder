f707b7f83daa469ff38278afb5d74950fe0a79ac
Abhishek Singh <hritik6058@gmail.com>
2024-01-13 18:15:04 +0530

    added basic change

--- src/components/flow-zone/index.tsx
+++ src/components/flow-zone/index.tsx
@@ -47,16 +47,17 @@ const FlowZone = () => {
 
   return (
     <div>
-    <h1>
-      Hola THere this is the element
-    </h1>
-    <h2>
-      Some other title
-      <button
-      onClick={some}>
-        Click on me
-      </button>
-    </h2>
+      <div className="relative flex-1 overflow-hidden">
+  <div className="absolute inset-0 transition-all h-full delay-200 duration-500">
+    <svg viewBox="0 0 2000 2222" xmlns="http://www.w3.org/2000/svg" className="customizer-design-preview !absolute inset-0 h-full w-full object-contain object-center transition-all delay-200 duration-500" >
+      <image href="https://design.sonsupply.com/img/garments/cotton-hoodie/Hoodie_LightGreyMelange.png" x="0" y="0" width="100%" height="100%"></image>
+      <image href="https://design.sonsupply.com/_nuxt/neck-label-large.7d8dfb3c.png" x="930" y="339" width="95" height="51" className=""></image>
+      <image href="https://storage.googleapis.com/son_supply_backend/uploads/83667dd1-09cb-46e5-bcef-52d2764ac330-1705131675065-3491358.svg" x="937.125" y="346.125" width="80.75" height="36.75" className=""></image>
+      {/* <!----> */}
+      {/* <!----> */}
+    </svg>
+  </div>
+</div>
     </div>
   )
 
