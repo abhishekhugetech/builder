5b3c82f31d0e992699e8b148d9f531e7c7562869
Abhishek Singh <hritik6058@gmail.com>
2024-01-20 01:30:10 +0530

    added key in looping component (color.tsx)

--- src/components/sidebar/editor/nodes/color.tsx
+++ src/components/sidebar/editor/nodes/color.tsx
@@ -4,42 +4,47 @@ import { CustomizationEditorProps } from "..";
 import CustomCard from "../../../colorbox/colorbox";
 import { ClothColorPair } from "../../../clothing/typings";
 
-
 const ColorNodeDataEditor: FC<CustomizationEditorProps> = (prop) => {
   const onColorSelected = (color: ClothColorPair) => {
-    prop.cloth.color = color.name
-    prop.onUpdateCloth(prop.cloth)
-  }
+    prop.cloth.color = color.name;
+    prop.onUpdateCloth(prop.cloth);
+  };
   return (
     <div>
       <div
-      css={css`
-        padding: 16px 28px;
-        border-bottom: 1px solid lightblue;
-        font-weight: 500;
-      `}
-    >
-      <p>Choose from our selection of in-stock colors for the fastest delivery.</p>
-    </div>
+        css={css`
+          padding: 16px 28px;
+          border-bottom: 1px solid lightblue;
+          font-weight: 500;
+        `}
+      >
+        <p>
+          Choose from our selection of in-stock colors for the fastest delivery.
+        </p>
+      </div>
 
-    <div css={css`
-      margin-top: 30px;
-    `}>
-        {prop.cloth.colors.map((color)=>(
-          <div onClick={()=>{
-            // console.log(color);
-            onColorSelected(color)
-          }}>
-        <CustomCard
-        color={color.color}
-        title={color.name.toLocaleUpperCase()}
-        checked={prop.cloth.color == color.name}
-        />
+      <div
+        css={css`
+          margin-top: 30px;
+        `}
+      >
+        {prop.cloth.colors.map((color) => (
+          <div
+            onClick={() => {
+              // console.log(color);
+              onColorSelected(color);
+            }}
+            key={color.name}
+          >
+            <CustomCard
+              color={color.color}
+              title={color.name.toLocaleUpperCase()}
+              checked={prop.cloth.color == color.name}
+            />
           </div>
         ))}
 
         <br />
-
       </div>
     </div>
   );
